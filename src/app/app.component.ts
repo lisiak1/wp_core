9d3e398e5e22ff48610f8294b84db3307d8816f5
Matej Liskovec <matej.liskovec@admiral.at>
2024-07-18 08:21:20 +0200

    nav bar started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  @ViewChild(NavbarComponent) navbar: NavbarComponent | undefined;
   title = 'wp-core';
 }
