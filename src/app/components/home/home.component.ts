e5a4af22d81fea87f4d4f97dcabd758cdf155e75
Matej Liskovec <matej.liskovec@admiral.at>
2024-07-18 22:35:36 +0200

    home page and routing

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {
+  focus!: boolean;
+  focus1!: boolean;
+
+  constructor() {}
+}
