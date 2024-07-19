75a76619cbd0cacef8e4cfd3d995f62b6550a660
Matej Liskovec <matej.liskovec@admiral.at>
2024-07-19 14:46:43 +0200

    HashLocationStrategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
-import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [
     Location,
-    { provide: LocationStrategy, useClass: PathLocationStrategy },
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
   ],
   // other providers],
   bootstrap: [AppComponent],
